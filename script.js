959de2cba1ec5e5d05a3920f562106cbf4b53b21
Charlie <charlieboyle252@gmail.com>
2024-09-30 19:50:23 -0500

    JSON test project

---
+++ script.js
@@ -0,0 +1,26 @@
+const people =
+    `[
+        {
+            "name": "john",
+            "job": "doctor",
+            "location": "Dallas",
+            "rating": 8.5
+        },
+        {
+            "name": "Suzy",
+            "job": "accountant",
+            "location": "Seatle",
+            "rating": 8.8
+        },
+        {
+            "name": "Henry",
+            "job": "Engineer",
+            "location": "Boston",
+            "rating": 8.0
+        }
+    ]`
+
+    console.log(people)
+
+    console.log(JSON.parse(people))
+
